3d7f5363d5bed61869760f589f62a569fc18c126
Sarawut <sarawutu@scg.com>
2024-06-10 11:35:01 +0700

    add font Prompt

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
       fontFamily: {
         sans: ["var(--font-sans)"],
         mono: ["var(--font-geist-mono)"],
+        prompt: ["var(--font-prompt)"],
       },
     },
   },
